98ffb48a90baff08fad369f9bc2022b53f3183ee
John Wass <wassj@ctc.com>
2020-07-26 00:39:33 -0400

    lfs fetch

--- src/main/scala/com/github/jw3/example/Boot.scala
+++ src/main/scala/com/github/jw3/example/Boot.scala
@@ -5,6 +5,7 @@ import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Flow
 import io.cloudstate.operator.Watcher
 import skuber.api.client.{EventType, WatchEvent}
+import skuber.json.batch.format._
 import skuber.json.format._
 
 object Boot extends App {
@@ -21,6 +22,7 @@ object Boot extends App {
     Flow[WatchEvent[gvc.Resource]].map {
       case WatchEvent(EventType.ADDED, gv) =>
         println(s"gvc ${gv.name} added ${gv.spec.uri}")
+        client create Cloner.make(gv)
       case WatchEvent(EventType.MODIFIED, gv) =>
         println(s"gvc ${gv.name} modified")
       case WatchEvent(EventType.DELETED, gv) =>
---
+++ src/main/scala/com/github/jw3/example/Cloner.scala
@@ -0,0 +1,39 @@
+package com.github.jw3.example
+
+import com.github.jw3.example.gvc.{Resource => GVC}
+import skuber.batch.Job
+import skuber.{EnvVar, Pod, RestartPolicy, Volume}
+
+object Cloner {
+  private val mountPath = "/opt/app-root/mount"
+  private val datadir = "data"
+
+  def make(gvc: GVC): Job = {
+    val vol = Volume(
+      gvc.name,
+      Volume.GitRepo(
+        gvc.spec.uri,
+        Some(gvc.spec.ref),
+        Some(datadir)
+      )
+    )
+
+    val container = skuber.Container(
+      name = "lfs",
+      image = "jwiii/centos-python-36-centos7-lfs:latest",
+      volumeMounts = List(
+        Volume.Mount(
+          gvc.name,
+          mountPath
+        )
+      ),
+      workingDir = Some(s"$mountPath/$datadir"),
+      command = List("git", "lfs", "fetch")
+    )
+
+    val spec = Pod.Spec().addContainer(container).withRestartPolicy(RestartPolicy.Never).addVolume(vol)
+    val templateSpec = Pod.Template.Spec.named("lfs").withPodSpec(spec)
+
+    Job(gvc.name).withTemplate(templateSpec)
+  }
+}
